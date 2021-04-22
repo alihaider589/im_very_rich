5f2ca7dd8f7a70f88ed60fdf2e3f640338d4614a
alihaider589 <alihaider589@outlook.com>
2021-04-22 23:31:24 +0500

    finishing app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Image(
-          image: NetworkImage('https://static.remove.bg/remove-bg-web/2a274ebbb5879d870a69caae33d94388a88e0e35/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg'),
+          image:AssetImage('images/diamond.png'),
         ),
       ),
     ),
